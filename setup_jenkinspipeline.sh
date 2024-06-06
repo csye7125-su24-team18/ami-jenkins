34388a84713682c9bc621f8e92baf84ed8686631
harshneu404 <agrawal.harsh@northeastern.edu>
2024-06-06 11:54:51 -0400

    update variables

--- setup_jenkinspipeline.sh
+++ setup_jenkinspipeline.sh
@@ -1,3 +1,6 @@
+echo GITHUB_SSH_PRIVATE_KEY=${var.github_ssh_private_key} > /tmp/jenkins_env.sh
+echo DOCKER_USERNAME=${var.docker_username} >> /tmp/jenkins_env.sh
+echo DOCKER_PASSWORD=${var.docker_password} >> /tmp/jenkins_env.sh
 sudo mv /tmp/jenkins.groovy /var/lib/jenkins/init.groovy.d/jenkins.groovy
 sudo mv /tmp/jenkins_creds.groovy /var/lib/jenkins/init.groovy.d/jenkins_creds.groovy
 sudo chmod -R 755 /var/lib/jenkins/init.groovy.d/jenkins.groovy
