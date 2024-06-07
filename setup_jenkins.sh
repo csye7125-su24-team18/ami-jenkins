9e6d8d84a77b94e952a43603378baabb15b6fc8c
harshneu404 <144640353+harshneu404@users.noreply.github.com>
2024-06-07 00:22:26 -0400

    Update cred loc (#43)

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -1,12 +1,18 @@
 export GITHUB_SSH_PRIVATE_KEY=$(cat /tmp/gitsecret.txt)
 
 # Store environment variables in /etc/environment
+sudo mkdir -p /etc/jenkins
+sudo chmod 777 /etc/jenkins
+
+sudo sh -c "echo 'GITHUB_SSH_PRIVATE_KEY=${GITHUB_SSH_PRIVATE_KEY}' >> /etc/jenkins/github_ssh_key"
+sudo sh -c "echo 'DOCKER_USERNAME=${DOCKER_USERNAME}' >> /etc/jenkins/dockerUsername"
+sudo sh -c "echo 'DOCKER_PASSWORD=${DOCKER_PASSWORD}' >> /etc/jenkins/dockerPassword"
 sudo sh -c "echo 'GITHUB_SSH_PRIVATE_KEY=${GITHUB_SSH_PRIVATE_KEY}' > /etc/environment"
 sudo sh -c "echo 'DOCKER_USERNAME=${DOCKER_USERNAME}' >> /etc/environment"
 sudo sh -c "echo 'DOCKER_PASSWORD=${DOCKER_PASSWORD}' >> /etc/environment"
-sudo echo "${GITHUB_SSH_PRIVATE_KEY}" > /var/lib/jenkins/github_ssh_key
-sudo echo "${DOCKER_USERNAME}" > /var/lib/jenkins/dockerUsername
-sudo echo "${DOCKER_PASSWORD}" > /var/lib/jenkins/dockerPassword
+# sudo echo "${GITHUB_SSH_PRIVATE_KEY}" > /var/lib/jenkins/github_ssh_key
+# sudo echo "${DOCKER_USERNAME}" > /var/lib/jenkins/dockerUsername
+# sudo echo "${DOCKER_PASSWORD}" > /var/lib/jenkins/dockerPassword
 sudo mkdir -p /var/lib/jenkins/init.groovy.d
 sudo mv /tmp/*.groovy /var/lib/jenkins/init.groovy.d/
 # mv /tmp/config/jenkins /etc/sysconfig/jenkins
