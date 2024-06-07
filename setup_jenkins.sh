1ad0bef144a85641167c9c490460c6bade46c252
harshneu404 <144640353+harshneu404@users.noreply.github.com>
2024-06-06 21:46:48 -0400

    Update permissions (#42)
    * update Permissions

--- setup_jenkins.sh
+++ setup_jenkins.sh
@@ -4,9 +4,9 @@ export GITHUB_SSH_PRIVATE_KEY=$(cat /tmp/gitsecret.txt)
 sudo sh -c "echo 'GITHUB_SSH_PRIVATE_KEY=${GITHUB_SSH_PRIVATE_KEY}' > /etc/environment"
 sudo sh -c "echo 'DOCKER_USERNAME=${DOCKER_USERNAME}' >> /etc/environment"
 sudo sh -c "echo 'DOCKER_PASSWORD=${DOCKER_PASSWORD}' >> /etc/environment"
-echo "${GITHUB_SSH_PRIVATE_KEY}" > /var/lib/jenkins/github_ssh_key
-echo "${DOCKER_USERNAME}" > /var/lib/jenkins/dockerUsername
-echo "${DOCKER_PASSWORD}" > /var/lib/jenkins/dockerPassword
+sudo echo "${GITHUB_SSH_PRIVATE_KEY}" > /var/lib/jenkins/github_ssh_key
+sudo echo "${DOCKER_USERNAME}" > /var/lib/jenkins/dockerUsername
+sudo echo "${DOCKER_PASSWORD}" > /var/lib/jenkins/dockerPassword
 sudo mkdir -p /var/lib/jenkins/init.groovy.d
 sudo mv /tmp/*.groovy /var/lib/jenkins/init.groovy.d/
 # mv /tmp/config/jenkins /etc/sysconfig/jenkins
