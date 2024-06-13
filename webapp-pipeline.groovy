58f5466150b706a703142b9ec960b4b68017bb9f
harshneu404 <agrawal.harsh@northeastern.edu>
2024-06-13 02:19:19 -0400

    feat: add webhook to image

--- webapp-pipeline.groovy
+++ webapp-pipeline.groovy
@@ -8,15 +8,16 @@ def jobDslScript = '''
     github {
       id('webapp-pipeline')
       scanCredentialsId('github_pat')
-      repoOwner('cyse7125-su24-team18')
+      repoOwner('csye7125-su24-team18')
       repository('webapp-cve-processor.git')
       configure { node ->
-                node / traits / 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {}
-                node / traits / 'org.jenkinsci.plugins.github__branch__source.WebhookRegistrationTrait' {
-                    webhookEvents()
-                    token('webapp')
+                   <triggers>
+                    <com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger plugin="multibranch-scan-webhook-trigger@1.0.11">
+                      <spec></spec>
+                      <token>webapp</token>
+                    </com.igalg.jenkins.plugins.mswt.trigger.ComputedFolderWebHookTrigger>
+                  </triggers>
                 }
-            }
     }
   }
  
